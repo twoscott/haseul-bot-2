78de7218d7ed22fc3fe3a8273d2bac4a8ae03959
twoscott <twoscott99@gmail.com>
2022-06-05 18:38:10 +0100

    use LogText in ready handler

--- handler/ready.go
+++ handler/ready.go
@@ -4,22 +4,13 @@ import (
 	"log"
 
 	"github.com/diamondburned/arikawa/v3/gateway"
-	"github.com/twoscott/haseul-bot-2/config"
+	"github.com/twoscott/haseul-bot-2/utils/botutil"
 )
 
 var started = false
 
 func (h *Handler) Ready(ev *gateway.ReadyEvent) {
-	cfg := config.GetInstance()
-	logChannelID := cfg.Discord.LogChannelID
-
-	var err error
-	if !logChannelID.IsValid() {
-		log.Println("Configured log channel ID is invalid.")
-	} else {
-		_, err = h.Router.State.SendMessage(logChannelID, "Ready to *Go!~*")
-	}
-
+	_, err := botutil.LogText(h.Router.State, "Ready to *Go!~*")
 	if err != nil {
 		log.Println(err)
 	}
