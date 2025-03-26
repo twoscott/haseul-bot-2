6fafc1512c7c869f74afb7292f0ab3d530fe47db
twoscott <twoscott99@gmail.com>
2025-03-26 06:52:13 +0000

    use 0x0 colour for default embeds; too many themes now

--- utils/dctools/colour.go
+++ utils/dctools/colour.go
@@ -18,23 +18,21 @@ const (
 	WhiteColour   = 0xffffff
 	BlackColour   = 0x23272a
 	// EmbedBackColour is the default embed background colour Discord uses.
-	EmbedBackColour = 0x2b2d31
+	EmbedBackColour = 0x000000
 )
 
 var ErrInvalidHexColour = errors.New("invalid hexadecimal RGB value provided")
 
 // ColourInvalid returns whether a colour is either null or a default colour.
 func ColourInvalid(colour discord.Color) bool {
-	return colour == 0x000000 ||
-		colour == discord.DefaultEmbedColor ||
-		colour == discord.NullColor
+	return colour == discord.NullColor
 }
 
 // EmbedColour returns a Discord colour where default black colours are
 // replaced with the default embed background colour.
 func EmbedColour(colour discord.Color) discord.Color {
 	if ColourInvalid(colour) {
-		colour = EmbedBackColour
+		colour = 0x000000
 	}
 	return colour
 }
